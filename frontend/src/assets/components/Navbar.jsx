d04ac0813f0534565bee952dc8717b7c9196f797
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-10-17 20:55:30 +0530

    Update Navbar.jsx

--- frontend/src/assets/components/Navbar.jsx
+++ frontend/src/assets/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="flex items-center">
           <img 
-            src="src/assets/image/logo.jpg" 
+            src="https://res.cloudinary.com/dpwcvgpt3/image/upload/v1728299251/yofdgz4gnxuitk8gzt7d.jpg" 
             alt="Logo" 
             className="h-16 w-auto mr-3 rounded-full border-4 border-white" 
           />
